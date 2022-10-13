1e7bca7c6ad0fba54a0b5b893bbd2e832115a927
Mark Tripney <mark.tripney@icloud.com>
2022-10-13 23:23:12 +0100

    Adjust styling; add Tailwind Prettier plugin.

--- js/script.js
+++ js/script.js
@@ -37,12 +37,12 @@ const displayItems = () => {
         ${item.complete && "checked"}
         value="${item.id}"
         class="mr-4">
-        <span class="flex-1 ${item.complete && "opacity-30"}">
+        <span class="flex-1 ${item.complete && "opacity-40"}">
         ${item.itemName}
         </span>
         <button aria-label="Remove ${item.itemName}" value="${item.id}">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 hover:fill-red-200">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </button>
       </li>`
