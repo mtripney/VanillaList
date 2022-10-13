e18a4c8007748ec60202ed0e984c5f4fa3d8e3fb
Mark Tripney <mark.tripney@icloud.com>
2022-10-13 20:02:36 +0100

    Add toggle complete/incomplete functionality.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,13 @@ const getFromLocalStorage = () => {
   }
 };
 
+const markAsComplete = (id) => {
+  const itemRef = items.find((item) => item.id === id);
+  // Toggle item.complete on click
+  itemRef.complete = !itemRef.complete;
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+};
+
 const deleteItem = (id) => {
   items = items.filter((item) => item.id !== id);
   list.dispatchEvent(new CustomEvent('itemsUpdated'));
@@ -25,9 +32,15 @@ const displayItems = () => {
     .map(
       (item) => `
       <li class="flex items-center mb-2">
-        <input type="checkbox" class="mr-4">
-        <span class="flex-1">${item.itemName}</span>
-        <button aria-label="Remove ${item.itemName}" value="${item.id}">
+        <input type="checkbox" ${item.complete && 'checked'} value="${
+        item.id
+      }" class="mr-4">
+        <span class="flex-1 ${item.complete && 'opacity-30'}">${
+        item.itemName
+      }</span>
+        <button aria-label="Remove ${item.itemName}" value="${
+        item.id
+      }">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
@@ -65,6 +78,9 @@ list.addEventListener('click', (e) => {
   // .closest() queries whether parent is a button, in case SVG is clicked.
   if (e.target.closest('button')) {
     deleteItem(parseInt(e.target.closest('button').value, 10));
+  }
+  if (e.target.matches('input[type="checkbox"]')) {
+    markAsComplete(parseInt(e.target.value, 10));
   }
 });
 
