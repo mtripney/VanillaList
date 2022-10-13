a1a3509a5fb16792eb4c637186007657629405cd
Mark Tripney <mark.tripney@icloud.com>
2022-10-13 22:43:16 +0100

    Fix ESLint/Prettier conflicts; fix formatting.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const shoppingForm = document.querySelector('.shopping');
-const list = document.querySelector('.list');
+const shoppingForm = document.querySelector(".shopping");
+const list = document.querySelector(".list");
 let items = [];
 
 const sendToLocalStorage = () => {
-  localStorage.setItem('items', JSON.stringify(items));
+  localStorage.setItem("items", JSON.stringify(items));
 };
 
 const getFromLocalStorage = () => {
-  const localStorageItems = JSON.parse(localStorage.getItem('items'));
+  const localStorageItems = JSON.parse(localStorage.getItem("items"));
   if (localStorageItems.length) {
     // Spread items from local storage into state
     items.push(...localStorageItems);
-    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+    list.dispatchEvent(new CustomEvent("itemsUpdated"));
   }
 };
 
@@ -19,12 +19,12 @@ const markAsComplete = (id) => {
   const itemRef = items.find((item) => item.id === id);
   // Toggle item.complete on click
   itemRef.complete = !itemRef.complete;
-  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 };
 
 const deleteItem = (id) => {
   items = items.filter((item) => item.id !== id);
-  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 };
 
 const displayItems = () => {
@@ -32,22 +32,22 @@ const displayItems = () => {
     .map(
       (item) => `
       <li class="flex items-center mb-2">
-        <input type="checkbox" ${item.complete && 'checked'} value="${
-        item.id
-      }" class="mr-4">
-        <span class="flex-1 ${item.complete && 'opacity-30'}">${
-        item.itemName
-      }</span>
-        <button aria-label="Remove ${item.itemName}" value="${
-        item.id
-      }">
+        <input
+        type="checkbox"
+        ${item.complete && "checked"}
+        value="${item.id}"
+        class="mr-4">
+        <span class="flex-1 ${item.complete && "opacity-30"}">
+        ${item.itemName}
+        </span>
+        <button aria-label="Remove ${item.itemName}" value="${item.id}">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </button>
       </li>`
     )
-    .join('');
+    .join("");
   list.innerHTML = html;
 };
 
@@ -67,17 +67,17 @@ const handleSubmit = (e) => {
   // Clear the input field.
   e.currentTarget.reset();
   // Custom event...
-  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 };
 
-shoppingForm.addEventListener('submit', handleSubmit);
-list.addEventListener('itemsUpdated', displayItems);
-list.addEventListener('itemsUpdated', sendToLocalStorage);
+shoppingForm.addEventListener("submit", handleSubmit);
+list.addEventListener("itemsUpdated", displayItems);
+list.addEventListener("itemsUpdated", sendToLocalStorage);
 // Use event delegation to handle item deletion.
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   // .closest() queries whether parent is a button, in case SVG is clicked.
-  if (e.target.closest('button')) {
-    deleteItem(parseInt(e.target.closest('button').value, 10));
+  if (e.target.closest("button")) {
+    deleteItem(parseInt(e.target.closest("button").value, 10));
   }
   if (e.target.matches('input[type="checkbox"]')) {
     markAsComplete(parseInt(e.target.value, 10));
