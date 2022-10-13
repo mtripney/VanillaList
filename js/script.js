a3e670e796b1d0ca0374c4756061da550bb3ba0b
Mark Tripney <mark.tripney@icloud.com>
2022-10-13 18:45:23 +0100

    Set/get items to/from local storage.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,33 @@ const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 const items = [];
 
+const sendToLocalStorage = () => {
+  localStorage.setItem('items', JSON.stringify(items));
+};
+
+const getFromLocalStorage = () => {
+  const localStorageItems = JSON.parse(localStorage.getItem('items'));
+  if (localStorageItems.length) {
+    // Spread items from local storage into state
+    items.push(...localStorageItems);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+};
+
 const displayItems = () => {
   const html = items
-    .map((item) => `<li>${item.itemName}</li>`)
+    .map(
+      (item) => `
+      <li class="flex items-center mb-2">
+        <input type="checkbox" class="mr-4">
+        <span class="flex-1">${item.itemName}</span>
+        <button aria-label="Remove ${item.itemName}">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </button>
+      </li>`
+    )
     .join('');
   list.innerHTML = html;
 };
@@ -12,6 +36,8 @@ const displayItems = () => {
 const handleSubmit = (e) => {
   e.preventDefault();
   const itemName = e.currentTarget.item.value;
+  // Don't react to an empty submission.
+  if (!itemName) return;
   const item = {
     itemName,
     // Date.now() is unique every millisecond...
@@ -22,8 +48,12 @@ const handleSubmit = (e) => {
   items.push(item);
   // Clear the input field.
   e.currentTarget.reset();
-  displayItems();
-  console.log(`You have ${items.length} items in your basket.`);
+  // Custom event...
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
 };
 
 shoppingForm.addEventListener('submit', handleSubmit);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', sendToLocalStorage);
+
+getFromLocalStorage();
