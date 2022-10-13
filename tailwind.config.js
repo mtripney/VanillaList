324b0ce799e3fdc99d771137d5f9f50fbb760147
Mark Tripney <mark.tripney@icloud.com>
2022-10-13 18:31:59 +0100

    Add custom box-shadow.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './js/script.js'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        layered:
+          '0 1px 1px rgba(0,0,0,0.12), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.12), 0 8px 8px rgba(0,0,0,0.12), 0 16px 16px rgba(0,0,0,0.12)',
+      },
+    },
   },
   plugins: [],
 };
